873eaa75307a7ff876c0a03384db4249a54a0ac7
Sandeep Patel <96694484+sandptel@users.noreply.github.com>
2024-06-17 06:37:21 +0530

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
 
   outputs = { home-manager, nixpkgs, ... }@inputs:
   let
-    username = "demeter";
+    username = "roronoa";
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
